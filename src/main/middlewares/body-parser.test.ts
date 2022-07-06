22a05f4b646400d2b8d9108514259649e9bda57e
ericknathan <erick.capito@hotmail.com>
2022-07-06 11:14:12 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (request, response) => {
+      response.send(request.body)
+    })
+
+    const object = { name: 'Erick' }
+
+    await request(app)
+      .post('/test_body_parser')
+      .send(object)
+      .expect(object)
+  })
+})
