14d054185b333649cf87a456a33c48dd1688045d
ericknathan <erick.capito@hotmail.com>
2022-07-07 10:38:51 -0300

    test: create test to do list

---
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -0,0 +1,11 @@
+describe('SignIn Controller', () => {
+  test.todo('should return 400 status if no email is provided')
+  test.todo('should return 400 status if no password is provided')
+  test.todo('should return 400 status if an invalid email is provided')
+  test.todo('should return 401 status if invalid credentials are provided')
+  test.todo('should return 200 status if valid credentials is provided')
+  test.todo('should call EmailValidator with correct email')
+  test.todo('should return 500 status if EmailValidator throws')
+  test.todo('should return call Authentication with correct values')
+  test.todo('should return 500 status if Authentication throws')
+})
