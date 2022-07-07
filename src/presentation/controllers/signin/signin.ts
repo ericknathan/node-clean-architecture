10c3f62a774ae7c405713c09481979578f62b4d9
ericknathan <erick.capito@hotmail.com>
2022-07-07 10:57:53 -0300

    feat: ensure SignInController returns 400 if no email is provided

---
+++ src/presentation/controllers/signin/signin.ts
@@ -0,0 +1,19 @@
+import { HttpResponse, HttpRequest, Controller } from './signin-protocols'
+import { MissingParamError } from '../../errors'
+import { badRequest, serverError } from '../../helpers/http-helper'
+
+export class SignInController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['email', 'password']
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
