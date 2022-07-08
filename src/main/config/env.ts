f299f333f852df1cd2582a0e5ab3b4bd734890ce
ericknathan <erick.capito@hotmail.com>
2022-07-08 14:27:09 -0300

    fix: changed SignIn encrypter to JsonWebToken

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://root:123456@localhost:27017',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'e40b26eafd24e4b1ca98ff99fd4a3d5d'
 }
--- src/main/factories/signin.ts
+++ src/main/factories/signin.ts
@@ -7,14 +7,17 @@ import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
 import { AccessTokenMongoRepository } from '../../infra/db/mongodb/access-token-repository/access-token'
+import { JwtAdapter } from '../../infra/cryptography/jwt/jwt-adapter'
+import env from '../config/env'
 
 export const makeSignInController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BCryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
   const accountMongoRepository = new AccountMongoRepository()
   const accessTokenRepository = new AccessTokenMongoRepository()
-  const dbAuthenticateAccount = new DbAuthenticateAccount(accountMongoRepository, bcryptAdapter, bcryptAdapter, accessTokenRepository)
+  const dbAuthenticateAccount = new DbAuthenticateAccount(accountMongoRepository, bcryptAdapter, jwtAdapter, accessTokenRepository)
   const signInController = new SignInController(emailValidatorAdapter, dbAuthenticateAccount)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signInController, logMongoRepository)
