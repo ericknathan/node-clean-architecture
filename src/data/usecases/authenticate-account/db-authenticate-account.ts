d3b86c5d529aae1e1cb72d921ee69cd5c93991a5
ericknathan <erick.capito@hotmail.com>
2022-07-08 10:58:40 -0300

    feat: ensure DbAuthenticateAccount calls Encrypter with correct id

--- src/data/usecases/authenticate-account/db-authenticate-account.ts
+++ src/data/usecases/authenticate-account/db-authenticate-account.ts
@@ -1,12 +1,14 @@
-import { Comparer, AuthenticateAccount, AuthenticateAccountModel, GetAccountRepository, CredentialsModel } from './db-authenticate-account-protocols'
+import { Comparer, Encrypter, AuthenticateAccount, AuthenticateAccountModel, GetAccountRepository, CredentialsModel } from './db-authenticate-account-protocols'
 
 export class DbAuthenticateAccount implements AuthenticateAccount {
   private readonly getAccountRepository: GetAccountRepository
   private readonly comparer: Comparer
+  private readonly encrypter: Encrypter
 
-  constructor (getAccountRepository: GetAccountRepository, comparer: Comparer) {
+  constructor (getAccountRepository: GetAccountRepository, comparer: Comparer, encrypter: Encrypter) {
     this.getAccountRepository = getAccountRepository
     this.comparer = comparer
+    this.encrypter = encrypter
   }
 
   async authenticate (credentials: CredentialsModel): Promise<AuthenticateAccountModel> {
@@ -16,7 +18,7 @@ export class DbAuthenticateAccount implements AuthenticateAccount {
       const isValid = await this.comparer.compare(credentials.password, account.password)
 
       if (isValid) {
-        console.log(isValid)
+        await this.encrypter.encrypt(account.id)
       }
     }
 
