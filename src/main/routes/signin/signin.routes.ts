f219b36baf8f47be9e96b2de3fa26a4160683036
ericknathan <erick.capito@hotmail.com>
2022-07-08 11:42:26 -0300

    feat: add SignIn routes

---
+++ src/main/routes/signin/signin.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { adaptRoute } from '../../adapters/express-route-adapter'
+import { makeSignInController } from '../../factories/signin'
+
+export default (router: Router): void => {
+  router.post('/signin', adaptRoute(makeSignInController()))
+}
