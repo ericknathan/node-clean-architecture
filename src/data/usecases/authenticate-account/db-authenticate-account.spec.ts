121a78b6ccc74bdfbfcd4f9ec7f047f3b8e1f204
ericknathan <erick.capito@hotmail.com>
2022-07-08 10:30:40 -0300

    test: ensure DbAuthenticateAccount calls LoadAccountRepository with correct email

--- src/data/usecases/authenticate-account/db-authenticate-account.spec.ts
+++ src/data/usecases/authenticate-account/db-authenticate-account.spec.ts
@@ -1,5 +1,52 @@
+import { DbAuthenticateAccount } from './db-authenticate-account'
+import { GetAccountRepository, GetAccountRepositoryPayload, CredentialsModel } from './db-authenticate-account-protocols'
+
+const makeGetAccountRepository = (): GetAccountRepository => {
+  class GetAccountRepositoryStub implements GetAccountRepository {
+    async getByEmail (email: string): Promise<GetAccountRepositoryPayload> {
+      return Promise.resolve(makeFakeAccountData())
+    }
+  }
+
+  return new GetAccountRepositoryStub()
+}
+
+const makeFakeAccountData = (): GetAccountRepositoryPayload => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  password: 'valid_password'
+})
+
+const makeFakeCredentials = (): CredentialsModel => ({
+  email: 'any_email@mail.com',
+  password: 'valid_password'
+})
+
+interface SutTypes {
+  sut: DbAuthenticateAccount
+  getAccountRepositoryStub: GetAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getAccountRepositoryStub = makeGetAccountRepository()
+  const sut = new DbAuthenticateAccount(getAccountRepositoryStub)
+
+  return {
+    sut,
+    getAccountRepositoryStub
+  }
+}
+
 describe('DbAuthenticateAccount Usecase', () => {
-  test.todo('should call LoadAccountRepository with correct email')
+  test('should call LoadAccountRepository with correct email', async () => {
+    const { sut, getAccountRepositoryStub } = makeSut()
+    const getAccountByEmailSpy = jest.spyOn(getAccountRepositoryStub, 'getByEmail')
+
+    const credentials = makeFakeCredentials()
+    await sut.authenticate(credentials)
+    expect(getAccountByEmailSpy).toHaveBeenCalledWith(credentials.email)
+  })
+
   test.todo('should throw LoadAccountRepository throws')
   test.todo('should return null if LoadAccountRepository returns null')
   test.todo('should call Comparer with correct values')
