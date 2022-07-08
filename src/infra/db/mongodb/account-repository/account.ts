2e055bdd53e6482313df27f99a5df1295281f81b
ericknathan <erick.capito@hotmail.com>
2022-07-08 13:59:09 -0300

    feat: ensure DbAccountRepository getByEmail returns null if account not exists

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -18,6 +18,8 @@ export class AccountMongoRepository implements AddAccountRepository, GetAccountR
     const accountCollection = await MongoHelper.getCollection('accounts')
     const accountByEmail = await accountCollection.findOne({ email })
 
+    if (!accountByEmail) return null
+
     return MongoHelper.map(accountByEmail) as AccountModel
   }
 }
