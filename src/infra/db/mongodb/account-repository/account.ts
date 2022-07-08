2aecab2e82866e8e0b2b704cb4369f67df648803
ericknathan <erick.capito@hotmail.com>
2022-07-08 11:14:47 -0300

    feat: implements getByEmail function on AccountMongoRepository

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { GetAccountRepository } from '../../../../data/protocols/get-account-repository'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { AccountModel } from '../../../../domain/models/account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, GetAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
@@ -11,5 +12,12 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountById = await accountCollection.findOne({ _id: insertedId })
 
     return MongoHelper.map(accountById) as AccountModel
+  }
+
+  async getByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const accountByEmail = await accountCollection.findOne({ email })
+
+    return MongoHelper.map(accountByEmail) as AccountModel
   }
 }
