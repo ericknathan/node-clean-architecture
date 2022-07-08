41791992b8c6d2f80f5376d920b616d40b5f1476
ericknathan <erick.capito@hotmail.com>
2022-07-08 10:12:32 -0300

    feat: create hash comparer on BCryptAdapter

--- src/infra/cryptography/bcrypt/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt/bcrypt-adapter.ts
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt'
+import { Comparer } from '../../../data/protocols/comparer'
 import { Encrypter } from '../../../data/protocols/encrypter'
 
-export class BCryptAdapter implements Encrypter {
+export class BCryptAdapter implements Encrypter, Comparer {
   private readonly salt: number
 
   constructor (salt: number) {
@@ -11,5 +12,9 @@ export class BCryptAdapter implements Encrypter {
   async encrypt (value: string): Promise<string> {
     const hash = await bcrypt.hash(value , this.salt)
     return hash
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(value, hash)
   }
 }
