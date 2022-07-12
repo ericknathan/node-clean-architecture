7f9a118b58d20b5eebd0e90d0db21eb5d081fb1d
ericknathan <erick.capito@hotmail.com>
2022-07-12 13:38:21 -0300

    feat: implements Comparer on CryptoAdapter

--- src/infra/cryptography/crypto/crypto-adapter.ts
+++ src/infra/cryptography/crypto/crypto-adapter.ts
@@ -1,9 +1,15 @@
 import crypto from 'crypto'
+import { Comparer } from '../../../data/protocols/comparer'
 import { Encrypter } from '../../../data/protocols/encrypter'
 
-export class CryptoAdapter implements Encrypter {
+export class CryptoAdapter implements Encrypter, Comparer {
   async encrypt (value: string): Promise<string> {
     const hash = crypto.createHash('sha256').update(value).digest('hex')
     return hash
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    const hashedValue = await this.encrypt(value)
+    return hashedValue === hash
   }
 }
