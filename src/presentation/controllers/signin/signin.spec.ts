dcabd91760c28b4def69d11f0781e07f7bbaa910
ericknathan <erick.capito@hotmail.com>
2022-07-18 09:26:08 -0300

    test: ensure SignInController calls Authentication with correct values

--- src/presentation/controllers/signin/signin.spec.ts
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -1,3 +1,4 @@
+import { Authentication } from '../../../domain/usecases/authentication'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, serverError } from '../../helpers/http-helper'
 import { EmailValidator, HttpRequest } from '../signup/signup-protocols'
@@ -13,6 +14,7 @@ const makeHttpRequest = (): HttpRequest => ({
 interface SutTypes {
   sut: SignInController
   emailValidatorStub: EmailValidator
+  authenticationStub: Authentication
 }
 
 const makeEmailValidator = (): EmailValidator => {
@@ -25,13 +27,25 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
+const makeAuthentication = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth (email: string, password: string): Promise<string> {
+      return Promise.resolve('any_token')
+    }
+  }
+
+  return new AuthenticationStub()
+}
+
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
+  const authenticationStub = makeAuthentication()
+  const sut = new SignInController(emailValidatorStub, authenticationStub)
 
-  const sut = new SignInController(emailValidatorStub)
   return {
     sut,
-    emailValidatorStub
+    emailValidatorStub,
+    authenticationStub
   }
 }
 
@@ -83,5 +97,16 @@ describe('Login Controller', () => {
 
     const httpResponse = await sut.handle(makeHttpRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('should call Authentication with correct values', async () => {
+    const { sut, authenticationStub } = makeSut()
+    const authSpy = jest.spyOn(authenticationStub, 'auth')
+
+    const httpRequest = makeHttpRequest()
+    await sut.handle(httpRequest)
+
+    const { email, password } = httpRequest.body
+    expect(authSpy).toHaveBeenCalledWith(email, password)
   })
 })
