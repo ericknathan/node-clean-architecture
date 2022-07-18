3bac28b575f75948070d1b2d8335c859aeeb0657
ericknathan <erick.capito@hotmail.com>
2022-07-18 09:45:58 -0300

    feat: ensure SignInController returns 200 if valid credentials are provided

--- src/presentation/controllers/signin/signin.ts
+++ src/presentation/controllers/signin/signin.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from './signin-protocols'
 
 export class SignInController implements Controller {
@@ -29,6 +29,10 @@ export class SignInController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
+
+      return ok({
+        accessToken
+      })
     } catch (error) {
       return serverError(error)
     }
