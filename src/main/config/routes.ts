6abe31892496cc23e9b37f26fe66350c443e312d
ericknathan <erick.capito@hotmail.com>
2022-07-19 10:43:17 -0300

    refactor: move routes and factories to a exclusive directory

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,5 +4,5 @@ import fastGlob from 'fast-glob'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fastGlob.sync('**/src/main/routes/**.routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+  fastGlob.sync('**/src/main/routes/**/**.routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
 }
