16b50f14abcfbb4ce911dae1334f6cd05e7df368
ericknathan <erick.capito@hotmail.com>
2022-07-21 11:59:48 -0300

    refactor: move data layer interfaces to folders

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,5 +1,5 @@
 import { Authentication, AuthenticationCredentials } from '../../../domain/usecases/authentication'
-import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { LoadAccountByEmailRepository } from '../../protocols/db/account/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (
