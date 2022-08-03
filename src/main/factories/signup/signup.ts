ecf8ee014f81a75d2833edc5a00cd7e550958305
ericknathan <erick.capito@hotmail.com>
2022-08-03 09:35:14 -0300

    fix: fields validation bug

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,19 +1,19 @@
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
-import { EmailValidatorAdapter } from '../../../utils/validators/email/email-validator-adapter'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BCryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { Controller } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
-  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const validation = makeSignUpValidation()
   const bcryptAdapter = new BCryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, emailValidatorAdapter)
+  const signUpController = new SignUpController(dbAddAccount, validation)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
